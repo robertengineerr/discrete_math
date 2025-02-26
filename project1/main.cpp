50062acd9518eca67087ca143ccf8a48fc034f3a
EC2 Default User <ec2-user@ip-172-31-20-18.us-east-2.compute.internal>
2025-02-26 20:47:01 +0000

    small changes. not done yet

--- project1/main.cpp
+++ project1/main.cpp
@@ -7,7 +7,12 @@
 #include <cmath>
 
 
-
+long long fact(int n, int count){
+    if(n>1) return fact(n-1, count)*n; 
+    else if(n==0) return 1;
+    else if(n<0) return 0;
+    return n;
+}
 
 int main(){
     int n=0;
@@ -15,14 +20,21 @@ int main(){
     
     //find the number of trailing zeros first
     //trailing zeros are only made with a 2 and 5 in the prime factorization
+    //there are always more fives than two unless the n=2
     //we can divide by five until it's indivisible.
     int count=0;
     for(int i=5; n/i>=1; i*=5) count+=n/i;
     
-    std::cout<<count;
+    std::cout<<count<<std::endl;
     
     //we can use the count to help us calculate the factorial without the trailing zeros
+    long long f=fact(n, count);
     
+    //from there we can find the sum of the digits by looping through the digits and adding the rightmost place
+    int sum=0;
+    for(int i=f; i>0; i=floor(i/10)) sum+=i%10;
+    
+    std::cout<<sum<<std::endl;
     
     return 0;
 }
