2bf7decb5b6e547c69782658a0313f520e35b27c
EC2 Default User <ec2-user@ip-172-31-20-18.us-east-2.compute.internal>
2025-02-27 01:48:01 +0000

    Finished work. May rewrite comments later.

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,17 +1,22 @@
-//MATH-2165-001
-//Robert Thomas
-//Project 1
-//Feb 26, 2025
+/*MATH-2165-001
+Robert Thomas
+Project 1
+Feb 26, 2025
+Description: This program takes an integer as input from the user and calculates
+the number of trailing zeros and the number of digits from the factorial
+without calculating the factorial directly.
+*/
 
 #include <iostream>
 #include <cmath>
 
-
-long long fact(int n, int count){
-    if(n>1) return fact(n-1, count)*n; 
-    else if(n==0) return 1;
-    else if(n<0) return 0;
-    return n;
+unsigned long long fact_no_zeros(int n){
+    unsigned long long r=1;
+    for(n; n>1; n--){
+        if(n%10) r*=n;
+        else r*=n/10;
+    }
+    return r;
 }
 
 int main(){
@@ -19,22 +24,18 @@ int main(){
     std::cin>>n;
     
     //find the number of trailing zeros first
-    //trailing zeros are only made with a 2 and 5 in the prime factorization
-    //there are always more fives than two unless the n=2
-    //we can divide by five until it's indivisible.
+    //trailing zeros are only made with a 2 and 5 in the prime factorization since 2*5=10
+    //there are always more fives than twos unless n=2
+    //thus we can divide by five until it's indivisible
+    //we'll count the number of fives along the way to get the power of 10 which is the number of zeros
     int count=0;
     for(int i=5; n/i>=1; i*=5) count+=n/i;
     
-    std::cout<<count<<std::endl;
+    unsigned long long f=fact_no_zeros(n);
+    unsigned long long sum=0;
+    for(unsigned long long i=f; i>0; i=floor(i/10)) sum+=i%10;
     
-    //we can use the count to help us calculate the factorial without the trailing zeros
-    long long f=fact(n, count);
+    std::cout<<sum<<count;
     
-    //from there we can find the sum of the digits by looping through the digits and adding the rightmost place
-    int sum=0;
-    for(int i=f; i>0; i=floor(i/10)) sum+=i%10;
-    
-    std::cout<<sum<<std::endl;
-    
-    return 0;
+    return 0; 
 }
